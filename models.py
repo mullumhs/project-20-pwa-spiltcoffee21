f239e1f03c0e88f7cd66e1f881ed1f257b5442b3
spiltcoffee21 <140124277+spiltcoffee21@users.noreply.github.com>
2024-11-26 14:58:57 +1100

    kill john

--- models.py
+++ models.py
@@ -4,3 +4,14 @@ db = SQLAlchemy()
 
 # Define your database model here
 # Example: class Item(db.Model):
+
+class Song(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    artist = db.Column(db.String(100), nullable=False)
+    title = db.Column(db.Text)
+    length = db.Column(db.interger, nullable=False)
+    genre = db.Column(db.Integer, nullable=False)
+    rating = db.Column(db.string, default=False)
+
+    def __repr__(self):
+        return f'<Fish {self.Fish}>'
